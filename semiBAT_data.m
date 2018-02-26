bd269bfd051452b2133c8581c2e008bd7186494c
Bokai Cao <caobokai@qq.com>
2018-02-26 08:48:45 -0600

    Add files via upload

--- semiBAT_data.m
+++ semiBAT_data.m
@@ -1,13 +1,13 @@
 function [X, Y] = semiBAT_data(m, t, n, k)
+    B = randn(m, k);
+    T = randn(t, k);
+    A = randn(n, k);
+    X = ktensor({B, B, T, A});
 
-B = randn(m,k);
-T = randn(t,k);
-A = randn(n,k);
-X = ktensor({B,B,T,A});
+    Y = zeros(n, 2);
+    l = ceil(n / 2);
+    Y(1 : l, 1) = 1;
+    Y(l + 1 : end, 2) = 1;
 
-Y = zeros(n,2);
-l = ceil(n/2);
-Y(1:l,1) = 1;
-Y(l+1:end,2) = 1;
-
-X = tensor(X);
+    X = tensor(X);
+end
